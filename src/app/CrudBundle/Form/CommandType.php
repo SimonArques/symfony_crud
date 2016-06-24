59aecab2f54e138ee4136ce15d89e83ae0590dc3
SimonArques <simonarques@gmail.com>
2016-06-24 15:33:53 +0200

    CRUD generate

---
+++ src/app/CrudBundle/Form/CommandType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace app\CrudBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class CommandType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('ref')
+            ->add('dateCreated')
+            ->add('customers')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'app\CrudBundle\Entity\Command'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'app_crudbundle_command';
+    }
+}
